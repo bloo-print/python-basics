4ce1b2429b0e9fcd631d68f05ea870dd3681fef1
Ken Chen <lbkchen@gmail.com>
2017-06-13 22:29:43 -0700

    First assignment.

---
+++ assignment.py
@@ -0,0 +1,21 @@
+def fib_recursive(n):
+    """
+    Write a recursive function to compute the n-th Fibonacci
+    number, beginning with n=0 (1) and n=1 (1).
+    >>> fib_recursive(2)
+    2
+    >>> fib_recursive(5)
+    8
+    """
+    pass
+
+def fib_iterative(n):
+    """
+    Write an iterative function to compute the n-th Fibonacci
+    number, beginning with n=0 (1) and n=1 (1).
+    >>> fib_iterative(2)
+    2
+    >>> fib_iterative(5)
+    8
+    """
+    pass
